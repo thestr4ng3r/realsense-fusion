db98af09d45547ba125170d364d2a6a9419d0203
Florian Märkl <info@florianmaerkl.de>
2018-12-08 16:28:38 +0100

    Basic Visualization with PCL

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,68 @@
 
-#include <stdio.h>
+#include <librealsense2/rs.hpp>
+
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr points_to_pcl(const rs2::points& points);
 
 int main()
 {
-	printf("fjds\n");
-	return 0;
+	try
+	{
+		rs2::pointcloud pc;
+		rs2::points points;
+
+		rs2::pipeline pipe;
+		pipe.start();
+
+		pcl::visualization::CloudViewer cloud_viewer("Cloud Viewer");
+		while(!cloud_viewer.wasStopped())
+		{
+			auto frames = pipe.wait_for_frames();
+			auto depth = frames.get_depth_frame();
+			points = pc.calculate(depth);
+
+			/*auto color = frames.get_color_frame();
+			// For cameras that don't have RGB sensor, we'll map the pointcloud to infrared instead of color
+			if (!color)
+				color = frames.get_infrared_frame();
+			pc.map_to(color);*/
+
+			auto pcl_points = points_to_pcl(points);
+
+			cloud_viewer.showCloud(pcl_points);
+		}
+
+		return 0;
+	}
+	catch (const rs2::error & e)
+	{
+		std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
+		return 1;
+	}
+}
+
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr points_to_pcl(const rs2::points& points)
+{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+	auto sp = points.get_profile().as<rs2::video_stream_profile>();
+	cloud->width = sp.width();
+	cloud->height = sp.height();
+	cloud->is_dense = false;
+	cloud->points.resize(points.size());
+	auto ptr = points.get_vertices();
+	for (auto& p : cloud->points)
+	{
+		p.x = -ptr->x;
+		p.y = -ptr->y;
+		p.z = ptr->z;
+		ptr++;
+	}
+
+	return cloud;
 }
