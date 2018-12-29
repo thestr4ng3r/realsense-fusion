28fcd3248c0c481702c8b989d5785bbba73ff703
Vulpes <laikan.fox@web.de>
2018-12-29 13:49:30 +0100

    init PC_Integrator

---
+++ include/pc_integrator.h
@@ -0,0 +1,20 @@
+
+#ifndef _PC_INTEGRATOR_H
+#define _PC_INTEGRATOR_H
+
+#include <GL/glew.h>
+
+class GLModel;
+class Window;
+
+class PC_Integrator
+{
+	private:
+
+	public:
+		PC_Integrator();
+		PC_Integrator();
+		
+};
+
+#endif //_PC_INTEGRATOR_H
---
+++ src/pc_integrator.cpp
@@ -0,0 +1,1 @@
+#include "pc_integrator.h"
